c8c3e3f8474bfa25912cb5ef01e287a0d28a5f2e
Kostya <lost.champ2007@gmail.com>
2023-05-14 20:04:00 +0500

    Получение User'ов по ID и Email(Баг фикс)

--- apps/api/src/users/user.api-gateway/user.gateway.command.ts
+++ apps/api/src/users/user.api-gateway/user.gateway.command.ts
@@ -1,13 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
-import { CreateUserContract } from '@kinopoisk-snitch/contracts';
+import { CreateUserDto } from '../dtos/create-user.dto';
 
 @Controller('/users')
 export class UserGatewayCommand {
   constructor(private readonly amqpConnection: AmqpConnection) {}
 
   @Post('/createUser')
-  async createUser(@Body() userDto: CreateUserContract.Request) {
+  async createUser(@Body() userDto: CreateUserDto) {
     try {
       const response = await this.amqpConnection.request({
         exchange: 'PostUsersExchange',
--- apps/api/src/users/user.api-gateway/user.gateway.query.ts
+++ apps/api/src/users/user.api-gateway/user.gateway.query.ts
@@ -1,4 +1,11 @@
-import { Body, Controller, Get, Param } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Param,
+} from '@nestjs/common';
 import { AmqpConnection } from '@golevelup/nestjs-rabbitmq';
 import { EmailUserContract } from '@kinopoisk-snitch/contracts';
 
@@ -7,13 +14,20 @@ export class UserGatewayQuery {
   constructor(private readonly amqpConnection: AmqpConnection) {}
 
   @Get('/getUser/:id')
-  async getUserById(@Param('id') user_id: number) {
-    const user = await this.amqpConnection.request({
-      exchange: 'GetUsersExchange',
-      routingKey: 'get-user',
-      payload: user_id,
-    });
-    return user;
+  async getUserById(@Param('id') user_id: string) {
+    if (isNaN(Number(user_id))) {
+      throw new HttpException(
+        'ID должен состоять из цифр',
+        HttpStatus.BAD_REQUEST
+      );
+    } else {
+      const user = await this.amqpConnection.request({
+        exchange: 'GetUsersExchange',
+        routingKey: 'get-user',
+        payload: user_id,
+      });
+      return user;
+    }
   }
 
   @Get('/getUserByEmail')
