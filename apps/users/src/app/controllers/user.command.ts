33fd4e26f5f0bcca5dd014ed6fc6d3309a5ab40a
Kostya <lost.champ2007@gmail.com>
2023-05-15 22:29:49 +0500

    Пофиксил баг с получением юзера по ID

--- apps/users/src/app/controllers/user.command.ts
+++ apps/users/src/app/controllers/user.command.ts
@@ -1,16 +1,13 @@
 import { Controller } from '@nestjs/common';
 import { UserService } from '../services/user.service';
-import { AmqpConnection, RabbitRPC } from '@golevelup/nestjs-rabbitmq';
+import { RabbitRPC } from '@golevelup/nestjs-rabbitmq';
 import { Payload } from '@nestjs/microservices';
 import { CreateUserContract } from '@kinopoisk-snitch/contracts';
 import { createUserRMQConfig } from '@kinopoisk-snitch/rmq-configs';
 
 @Controller()
 export class UserCommand {
-  constructor(
-    private readonly userService: UserService,
-    private readonly amqpConnection: AmqpConnection
-  ) {}
+  constructor(private readonly userService: UserService) {}
 
   @RabbitRPC(createUserRMQConfig())
   async createUser(@Payload() userDto: CreateUserContract.Request) {
