94f5a5b611cea765dfc8dd75545cc564ba3758ae
Igor Suvorov <suekandig@mail.ru>
2023-05-18 02:47:27 +0300

    create examples folder

--- migrations/examples/1-kinopoisk.ts
+++ migrations/examples/1-kinopoisk.ts
@@ -1,6 +1,6 @@
 import { MigrationInterface, QueryRunner } from "typeorm"
 
-export class Kinopoisk1684365532709 implements MigrationInterface {
+export class Kinopoisk1 implements MigrationInterface {
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
