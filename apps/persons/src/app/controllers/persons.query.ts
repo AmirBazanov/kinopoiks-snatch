5e6b1d8329721e4cf9d4a5d8ff265a6c2bb3eb82
Igor Suvorov <suekandig@mail.ru>
2023-05-19 17:31:17 +0300

    In GET personById added awards array

--- apps/persons/src/app/controllers/persons.query.ts
+++ apps/persons/src/app/controllers/persons.query.ts
@@ -15,37 +15,6 @@ export class PersonsQuery {
 
   @RabbitRPC(getPersonByIdRMQConfig())
   async getPersonById(@Payload() person_id: number) {
-    const person = await this.personService.getPersonById(person_id);
-
-    // const career = await this.amqpConnection.request({
-    //   exchange: getCareerArrayOfPerson().exchange,
-    //   routingKey: getCareerArrayOfPerson().routingKey,
-    //   payload: person_id,
-    // })
-
-    // const genres = await this.amqpConnection.request({
-    //   exchange: getGenresArrayOfPerson().exchange,
-    //   routingKey: getGenresArrayOfPerson().routingKey,
-    //   payload: person_id,
-    // })
-
-    const countMovies = 100; //= await this.amqpConnection.request({
-    //   exchange: getCountMoviesArrayOfPerson().exchange,
-    //   routingKey: getCountMoviesArrayOfPerson().routingKey,
-    //   payload: person_id,
-    // })
-
-    return {
-      fullName: person.name + ' ' + person.sur_name,
-      career: [],
-      genres: [],
-      height: person.height,
-      dateBirth: person.date_birth,
-      placeBirth: person.place_birth,
-      spouse: person.spouse,
-      photoLink: person.photo,
-      countMovies: countMovies,
-      isEng: person.is_eng
-    }
+    return await this.personService.getPersonById(person_id);
   }
 }
